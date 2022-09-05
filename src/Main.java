d54697b4991a9444cf973c9fbc761cefc22aee6a
hankukin_indae_coding_mothae <taeju0627@gmail.com>
2022-09-05 23:21:23 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
         // WordManger를 구현하기
         new WordMananger().start();
     }
